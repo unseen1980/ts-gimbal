0853e58f2aff7bbb413cf0928050935c78637ed8
unseen1980 <koutsiaris@gmail.com>
2018-10-03 22:33:18 +0100

    add tracking

--- src/app.ts
+++ src/app.ts
@@ -2,20 +2,59 @@ import express from 'express';
 import http from 'http';
 import socket from 'socket.io';
 import path from 'path';
+import five from 'johnny-five';
 
 const app = express();
 const server = new http.Server(app);
 const io = socket(server);
+const board: five.Board = new five.Board();
+
+interface IAxisValues {
+  x: number;
+  y: number;
+}
+
+// Arduino is up and running
+board.on('ready', () => {
+  const led: five.Led = new five.Led(13);
+  const servo1: five.Servo = new five.Servo(10);
+  const servo2: five.Servo = new five.Servo(11);
+
+  const centerCamera = (): void => {
+    servo1.center();
+    servo2.to(60);
+  };
+
+  // Move camera for Face tracking.
+  // TODO: Write a smart algorithm and remove hardcoded angle values
+  const moveCamera = (data: IAxisValues) => {
+    const servoX: number = data.x;
+    const servoY: number = data.y;
+
+    console.log(`Received x:${servoX} y:${servoY}`);
+
+    if (servoX < 180) servo1.to(95);
+    if (servoX > 270) servo1.to(85);
+    if (servoY < 80) servo2.to(70);
+    if (servoY > 200) servo2.to(60);
+
+    return;
+  }
+
+  // Websockets connection estabished
+  io.on('connection', socket => {
+    socket.on('change:interval', data => {
+      led.blink(data);
+      moveCamera(data)
+    });
+  });
+
+  centerCamera();
+
+});
 
 app.use('/', express.static(path.join(__dirname, 'client')))
 
-io.on('connection', (socket:any) => {
-  console.log('a user connected');
-  socket.on('hello', function(msg: string){
-    console.log('message: ' + msg);
-  });
-});
-
-server.listen(5000, function(){
+server.listen(5000, function () {
   console.log('listening on *:5000');
 });
--- src/client/main.js
+++ src/client/main.js
@@ -1,6 +1,31 @@
 window.onload = function() {
     const socket = io();
-    setInterval(() => {
-        socket.emit('hello', 'world!');
-    }, 1000);
+    const canvas = document.getElementById('canvas');
+    const context = canvas.getContext('2d');
+    const tracker = new tracking.ObjectTracker('face');
+
+    tracker.setInitialScale(4);
+    tracker.setStepSize(2);
+    tracker.setEdgesDensity(0.1);
+
+    tracking.track('#video', tracker, {
+        camera: true
+    });
+
+    tracker.on('track', ({ data }) => {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+
+        data.forEach(({ x, y, width, height }) => {
+            console.log(`x:${x},y:${y}`);
+
+            socket.emit("change:interval", { x, y });
+
+            context.strokeStyle = 'red';
+            context.strokeRect(x, y, width, height);
+            context.font = '18px Helvetica';
+            context.fillStyle = "red";
+            context.fillText(`x: ${x}px`, x + width + 10, y + 21);
+            context.fillText(`y: ${y}px`, x + width + 10, y + 32);
+        });
+    });
 }
